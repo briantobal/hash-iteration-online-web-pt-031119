dfea15a506b2c98bc4b13e55de9d403bdbb4c07a
Brian Tobal <brian.tobal@gmail.com>
2019-03-22 10:33:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,8 +8,8 @@ require 'pry'
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-  binding.pry 
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+   binding.pry
 end
 
 
